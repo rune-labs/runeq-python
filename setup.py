5c9dbc0c4215e47e8bf77a5440143cc97959e79b
Joshua Jones <joshua.jones.software@gmail.com>
2020-06-10 16:03:06 -0400

    add support for new pagination methodology (#5)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements/common.txt', 'r') as r:
 
 setup(
     name=package_name,
-    version='0.2.0',
+    version='0.3.0',
     author='Rune Labs',
     maintainer_email='support@runelabs.io',
     description='Query data from Rune Labs APIs',
