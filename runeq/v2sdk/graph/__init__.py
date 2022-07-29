d01dec4a9b1f89a3841733320d82f5b81190de39
Abhinav Pottabathula <apottabathula@gmail.com>
2022-07-28 18:05:50 -0700

    [DATA-317] Remove old V2 SDK (#18)
    * removing old v2 sdk
    
    * linting and fixing docs dependencies

--- runeq/v2sdk/graph/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Client for the Rune Labs graph API.
-
-This package is not intended to be used directly, and implementation is
-subject to change. Use the functionality exposed by the `runeq.session()`
-client session instead.
-
-"""
