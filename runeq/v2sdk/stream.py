bbb05467a02232fe666888ae218c185097bcc3e6
Abhinav Pottabathula <apottabathula@gmail.com>
2022-08-03 10:03:31 -0700

    Updating csv and json typing docs

--- runeq/v2sdk/stream.py
+++ runeq/v2sdk/stream.py
@@ -2,11 +2,17 @@
 V2 SDK functionality to support Stream operations.
 
 """
+from typing import Literal
+
 from runeq import errors
 from . import client
 
 
-def get_stream(stream_id: str, limit: int = 100, format: str = "csv"):
+def get_stream(
+    stream_id: str,
+    limit: int = 100,
+    format: Literal["csv", "json"] = "csv"
+):
     """
     Fetch stream data from the Stream API.
 
@@ -26,16 +32,16 @@ def get_stream(stream_id: str, limit: int = 100, format: str = "csv"):
     -------
     Generator over stream data in the specified format, defaulting to csv.
 
-    With a 'csv' format, each iteration of the generator returns column
-    names and up to 'limit' rows of data. For example:
+    With a 'csv' format, each iteration of the generator returns a string
+    with column names and up to 'limit' rows of data. For example:
 
-    time,acceleration,measurement_duration_ns
+    "time,acceleration,measurement_duration_ns
     1648412619.2589417,-0.00386,20000000
     1648412620.2600197,-0.00816,20000000
-    1648412621.2671237,-0.01420,20000000
+    1648412621.2671237,-0.01420,20000000"
 
-    With a 'json' format, each iteration of the generator returns cardinality
-    and data fields. For example:
+    With a 'json' format, each iteration of the generator returns a dict
+    with data fields and cardinality up to 'limit' data points. For example:
 
     {
         "cardinality": 3,
@@ -79,7 +85,11 @@ def get_stream(stream_id: str, limit: int = 100, format: str = "csv"):
     return client._stream_client.get_data(url, **params)
 
 
-def get_availability(stream_id: str, limit: int = 100, format: str = "csv"):
+def get_availability(
+    stream_id: str,
+    limit: int = 100,
+    format: Literal["csv", "json"] = "csv"
+):
     """
     Fetch stream data availability from the Stream API.
 
@@ -100,16 +110,16 @@ def get_availability(stream_id: str, limit: int = 100, format: str = "csv"):
     Generator over stream availability data in the specified format,
     defaulting to csv.
 
-    With a 'csv' format, each iteration of the generator returns a csv with
-    column names and up to 'limit' rows of data. For example:
+    With a 'csv' format, each iteration of the generator returns a string
+    with column names and up to 'limit' rows of data. For example:
 
-    time,availability
+    "time,availability
     1648412600,1
     1648412900,1
-    1648413200,0
+    1648413200,0"
 
-    With a 'json' format, each iteration of the generator returns a json with
-    cardinality and data fields. For example:
+    With a 'json' format, each iteration of the generator returns a dict
+    with data fields and cardinality up to 'limit' data points. For example:
 
     {
         "approx_available_duration_s": 900,
