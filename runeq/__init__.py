b94147c67154a52645af6f7441dc1d273a1383da
Abhinav Pottabathula <apottabathula@gmail.com>
2022-12-02 10:05:48 -0800

    supporting variety of import structures

--- runeq/__init__.py
+++ runeq/__init__.py
@@ -3,7 +3,7 @@ The Rune Labs Standard Development Kit (SDK) for querying data from Rune
 APIs.
 
 """
-from . import stream
+from . import stream, resources
 from .config import Config
 from .resources.client import initialize
 
@@ -11,5 +11,6 @@ from .resources.client import initialize
 __all__ = [
     'Config',
     'initialize',
+    'resources',
     'stream',
 ]
