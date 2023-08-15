34003031135b47f497975a0d2b112b5911f8769a
Carolyn Ranti <carolyn@runelabs.io>
2023-08-15 13:54:07 -0400

    add return type

--- runeq/resources/stream.py
+++ runeq/resources/stream.py
@@ -12,7 +12,7 @@ from .client import StreamClient, global_stream_client
 _time_type = Union[int, float, date, datetime]
 
 
-def _time_type_to_unix_secs(t: _time_type):
+def _time_type_to_unix_secs(t: _time_type) -> Union[int, float]:
     """Standardize time input as timestamp (in unix secs)"""
     if isinstance(t, datetime):
         return t.timestamp()
