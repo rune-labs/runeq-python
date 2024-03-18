41843afd0b7e3997ebe7512b2564a975b9a02053
Carolyn Ranti <carolyn@runelabs.io>
2024-03-18 10:50:16 -0400

    deprecate EOL python versions

--- setup.py
+++ setup.py
@@ -25,11 +25,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     entry_points={
         "console_scripts": [
