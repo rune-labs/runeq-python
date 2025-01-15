7aa97a2c8d1212b7b84aa00658116f8e8289bc63
Brian King <bking@runelabs.io>
2025-01-15 09:54:14 -0500

    lints and strive_url

--- runeq/resources/strive.py
+++ runeq/resources/strive.py
@@ -3,11 +3,10 @@ Query data directly from the Strive API.
 
 """
 
-from rich import print
-
-from .client import global_strive_client, StriveClient
 from datetime import date, timedelta
-from typing import List, Dict, Optional
+from typing import Dict, List, Optional
+
+from .client import StriveClient, global_strive_client
 
 
 def get_sleep_metrics(
@@ -24,10 +23,8 @@ def get_sleep_metrics(
     all_metrics = []
     chunk_size = timedelta(days=30)
     chunk_start = start_date
-    print(chunk_start, end_date)
     while chunk_start <= end_date:
         chunk_end = min(chunk_start + chunk_size, end_date)
-        print(chunk_start, chunk_end)
         resp = client.get(
             "/api/v3/sleep_metrics",
             params={
@@ -39,7 +36,6 @@ def get_sleep_metrics(
         # if non-200, raise exception
         resp.raise_for_status()
 
-        print("resp", resp.json())
         all_metrics.extend(resp.json()["data"]["sleep_metrics_healthkit"])
         chunk_start = chunk_end + timedelta(days=1)
 
