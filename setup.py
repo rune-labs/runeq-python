3b6db133a36780a64f4e662745b67a7a2e8ef345
Girish <girish@runelabs.io>
2025-07-18 11:53:11 -0400

    Bump Version 0.17.5 (#77)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements/common.txt", "r") as r:
 
 setup(
     name=package_name,
-    version="0.17.4",
+    version="0.17.5",
     author="Rune Labs",
     maintainer_email="support@runelabs.io",
     description="Query data from Rune Labs APIs",
